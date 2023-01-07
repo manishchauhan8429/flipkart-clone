32cc26ba1ee80ad181bf8f15da83b151dc6a5875
manish chauhan <manishchauhan8429@gmail.com>
2023-01-07 17:53:29 +0530

    code update

---
+++ cmp/layout/footer/allDetails.jsx
@@ -0,0 +1,16 @@
+const Alldetailes = ()=>(
+    <div className="mt-4 p-4 bg-[#172337]">
+     <div class="grid grid-cols-12 gap-2">
+        <div class="col-2"><div className="border w-100% bg-white p-5">0</div></div>
+        <div class="col-2"><div className="border w-100% bg-white p-5">1</div></div>
+        <div class="col-2"><div className="border w-100% bg-white p-5">2</div></div>
+        <div class="col-2"><div className="border w-100% bg-white p-5">3</div></div>
+        <div class="col-2"><div className="border w-100% bg-white p-5">4</div></div>
+        <div class="col-2"><div className="border w-100% bg-white p-5">5</div></div>
+        
+    </div>
+
+    
+    </div>
+)
+export default Alldetailes;
--- cmp/layout/footer/index.jsx
+++ cmp/layout/footer/index.jsx
@@ -1,28 +1,33 @@
+import Alldetailes from "./allDetails";
 import data from "./data"; 
+import FlipkartDetails from "./flipkartDetails";
 const Footer = ()=>(
-<div className="border pt-10 p-5 pb-0">
-    <p style={{color:"#787878"}} className="font-bold text-sm mb-3">Top Stories:
-        <span style={{color:"#515151"}} className="font-bold text-sm"> Brand Directory</span>
+<>
+<div className="border-t pt-10 p-5 pb-0">
+    <p  className="font-bold text-sm mb-3 text-[#787878]">Top Stories:
+        <span className="font-bold text-sm text-[#515151]"> Brand Directory</span>
     </p>
     {
         data.map((item,index)=>(
             <p 
                 key={index}
-                className="text-sm font-medium"
-                style={{color:"#787878"}}>{item.title}: 
+                className="text-sm font-medium text-[#787878]"
+                >{item.title}: 
                 {
                     item.subtitle.map((subItem,subIndex)=>(
                         <span 
                             key={subIndex}
-                            style={{color:"#878787",fontSize:"14px"}}
-                            className="text-current font-normal text-sm"> {subItem.desc}
+                            className="text-current font-normal font-sans text-[#878787] text-xs"> {subItem.desc}
                             <span className="text-neutral-900"> | </span>
                         </span>                                                                   
                     ))
                 }                       
             </p>
         ))
-    }      
+    }  
+    <FlipkartDetails />
 </div>
+ <Alldetailes />
+ </>
 )
 export default Footer;
